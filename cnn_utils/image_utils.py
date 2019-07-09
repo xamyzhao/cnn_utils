40855f6220c6c88c68dd40616b6a02bd76ad91ef
xamyzhao <xamyzhao@mit.edu>
2019-07-09 16:29:24 -0400

    improved pad_or_crop fn, no more reliance on opencv

--- cnn_utils/image_utils.py
+++ cnn_utils/image_utils.py
@@ -17,22 +17,36 @@ def pad_or_crop_to_shape(
     # an out_shape with a dimension value of None means just don't crop or pad in that dim
     border_size = [out_shape[d] - I.shape[d] if out_shape[d] is not None else 0 for d in range(2)]
 
-    if border_size[0] > 0:
-        I = cv2.copyMakeBorder(I,
-                               int(math.floor(border_size[0] / 2.0)),
-                               int(math.ceil(border_size[0] / 2.0)), 0, 0,
-                               cv2.BORDER_CONSTANT, value=border_color)
-    elif border_size[0] < 0:
-        I = I[-int(math.floor(border_size[0] / 2.0)): I.shape[0]
-                                                      + int(math.ceil(border_size[0] / 2.0)), :, :]
-    if border_size[1] > 0:
-        I = cv2.copyMakeBorder(I, 0, 0,
-                               int(math.floor(border_size[1] / 2.0)),
-                               int(math.ceil(border_size[1] / 2.0)),
-                               cv2.BORDER_CONSTANT, value=border_color)
-    elif border_size[1] < 0:
-        I = I[:, -int(math.floor(border_size[1] / 2.0)): I.shape[1]
-                                                         + int(math.ceil(border_size[1] / 2.0)), :]
+    if not border_size[0] == 0:
+        top_border = abs(int(math.floor(border_size[0] / 2.)))
+        bottom_border = abs(int(math.ceil(border_size[0] / 2.)))
+
+        if border_size[0] > 0:
+            # pad with rows on top and bottom
+            I = np.concatenate([
+                np.ones((top_border,) + I.shape[1:]) * border_color,
+                I,
+                np.ones((bottom_border,) + I.shape[1:]) * border_color
+            ], axis=0)
+
+        elif border_size[0] < 0:
+            # crop from top and bottom
+            I = I[top_border:-bottom_border]
+
+    if not border_size[1] == 0:
+        left_border = abs(int(math.floor(border_size[1] / 2.)))
+        right_border = abs(int(math.ceil(border_size[1] / 2.)))
+
+        if border_size[1] > 0:
+            # pad with cols on left and right
+            I = np.concatenate([
+                np.ones((I.shape[0], left_border) + I.shape[2:]) * border_color,
+                I,
+                np.ones((I.shape[0], right_border) + I.shape[2:]) * border_color,
+            ], axis=1)
+        elif border_size[1] < 0:
+            # crop left and right sides
+            I = I[:, left_border: -right_border]
 
     return I
 
@@ -102,3 +116,26 @@ def create_gaussian_kernel(sigma, n_sigmas_per_side=8, n_dims=2):
     #    cv2.imwrite('gauss_z.jpg', gauss_kernel_2d[:, :,gauss_kernel_2d.shape[2]/2 ]*255)
     # gauss_kernel_2d = np.reshape(gauss_kernel_2d, gauss_kernel_2d.shape + (1,1))
     return gauss_kernel_2d
+
+
+def _test_pad_or_crop_to_shape():
+    I = np.ones((50, 50, 3)) * 255
+
+    target_shape = (70, 80)
+    border_color = (255, 0, 0)
+    I_padded = pad_or_crop_to_shape(I, target_shape, border_color)
+    assert I_padded.shape[0] == target_shape[0]
+    assert I_padded.shape[1] == target_shape[1]
+    assert np.all(I_padded[0, 0] == border_color)
+    assert np.all(I_padded[25, 25] == (255, 255, 255))
+    print('pad_or_crop_to_shape padding test: PASSED')
+
+    target_shape = (40, 30)
+    I_padded = pad_or_crop_to_shape(I, target_shape, border_color)
+    assert I_padded.shape[0] == target_shape[0]
+    assert I_padded.shape[1] == target_shape[1]
+    assert np.all(I_padded[0, 0] == (255, 255, 255))
+    print('pad_or_crop_to_shape cropping test: PASSED')
+
+if __name__ == '__main__':
+    _test_pad_or_crop_to_shape()
